4b31c6aa4cb119c312fff50891107a2ae1f9523f
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-08-19 11:46:47 +0100

    un-fucked Usync changes (deleted CSHTML)

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "5525b6a5a83cb777")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.8")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedModels
 {
