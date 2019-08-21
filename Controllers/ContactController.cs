f2e90d143f355f579e809ef9a7e84e27ca24efb6
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-08-21 13:56:53 +0100

    stopping for now. url of tut in comment

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Web.Mvc;
 using Umbraco.Web.Mvc;
 using PJDu8.Models;
-
+using System.Net.Mail;
 namespace PJDu8.Controllers
 {
     public class ContactController : SurfaceController
@@ -11,10 +11,32 @@ namespace PJDu8.Controllers
         {
             return $"~/Views/Partials/Contact/{name}.cshtml";
         }
+        [HttpGet]
         public ActionResult RenderForm()
         {
 
             return PartialView(GetViewPath("_ContactForm"), new ContactViewModel());
+        }
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView(GetViewPath("ContactForm"), model);
+        }
+        [HttpPost]
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+             if (ModelState.IsValid)
+            {
+                success = SendEmail(model);
+            }
+            return PartialView(GetViewPath(success ? "_Success" : "_Error"));
+        }
+        public bool SendEmail(ContactViewModel model)
+        {
+            //code for send email
+            return true;
+            // https://youtu.be/g3pVRW5nF4U?list=PL90L_HquhD-_Wu55PJ9N8mZ6Lh5Rhy0X6&t=1318
         }
     }
 }
