b7a332542d3f05a0046d766f8434e9800baceb98
Patrick Davis <patrick_davis97@hotmail.co.uk>
2019-08-27 22:08:42 +0100

    Web config bug

--- App_Data/Models/models.generated.cs
+++ App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "ee44c0598e037d1e")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.3")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedModels
 {
